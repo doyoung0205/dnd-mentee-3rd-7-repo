b8e835e62d79f8a1cf47bc3a325e4d04bef90a08
KDY <doyoung0205@naver.com>
2020-08-06 23:34:27 +0900

    REF SignModal 구조 변경

--- frontend/src/components/sign/SignInContainer.vue
+++ frontend/src/components/sign/SignInContainer.vue
@@ -1,49 +1,70 @@
 <template>
-  <SignLayout>
-    <template #header>
-      <header class="ImageContainer"></header>
-    </template>
-    <template #content>
-      <div class="FormContainer RegisterFormContainer">
-        <form
-          v-on:submit.prevent="submitForm"
-          class="RegisterFormContainer__FormTag"
-        >
-          <div class="EmailInputContainer">
-            <label
-              class="FormContainer__label RegisterFormContainer__EmailLabel"
-              for="email"
-              >이메일</label
+  <div class="FormContainer RegisterFormContainer">
+    <form v-on:submit.prevent="onSubmit" class="RegisterFormContainer__FormTag">
+      <transition name="slide-left">
+        <div class="EmailInputContainer">
+          <label
+            class="FormContainer__label RegisterFormContainer__EmailLabel"
+            for="email"
+            >이메일</label
+          >
+          <input
+            class="FormContainer__input"
+            id="email"
+            type="text"
+            v-model="email"
+          />
+          <div class="RegisterErrorContainer">
+            <p
+              class="RegisterErrorContainer__Text RegisterErrorContainer__Text-shadow"
             >
-            <input class="FormContainer__input" id="email" type="text" />
-            <div class="RegisterErrorContainer">
-              <p
-                class="RegisterErrorContainer__Text RegisterErrorContainer__Text-shadow"
-              >
-                {{ logMessage }}
-              </p>
-            </div>
+              {{ logMessage }}
+            </p>
+          </div>
+        </div>
+      </transition>
+
+      <transition name="slide-left">
+        <div class="PWInputContainer">
+          <div class="RegisterErrorContainer">
+            <p class="RegisterErrorContainer__Text">
+              {{ logMessage }}
+            </p>
           </div>
 
-          <button class="FormContainer__button RegisterFormContainer__button">
-            ㅁㄴㅇㄹㅁ
-          </button>
-        </form>
-      </div>
-    </template>
-  </SignLayout>
+          <label
+            class="FormContainer__label RegisterFormContainer__labelPassword RegisterFormContainer__label-darkgreen"
+            for="password"
+            >비밀번호
+          </label>
+          <input
+            class="FormContainer__input"
+            id="password"
+            type="text"
+            v-model="password"
+          />
+        </div>
+      </transition>
+
+      <button
+        class="FormContainer__button RegisterFormContainer__button"
+        @submit.prevent="submitForm"
+      >
+        로그인
+      </button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import SignLayout from "@/components/sign/SignLayout.vue";
 import Vue from "vue";
 export default Vue.extend({
   name: "SignInContainer",
-  components: {
-    SignLayout
-  },
+  components: {},
   data() {
     return {
+      email: "",
+      password: "",
       logMessage: ""
     };
   },
