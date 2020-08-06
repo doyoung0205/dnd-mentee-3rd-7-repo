1cf44636aeb7eca821ae28331a8743564f8779ac
KDY <doyoung0205@naver.com>
2020-08-07 00:22:49 +0900

    Feat  - Add Sign Header markup

--- frontend/src/components/sign/SignModal.vue
+++ frontend/src/components/sign/SignModal.vue
@@ -2,7 +2,12 @@
   <div class="signModal">
     <SignLayout>
       <template #header>
-        <header class="ImageContainer"></header>
+        <div id="signModal__header__wrap">
+          <div class="signModal__header__container">
+            <div class="signModal__header__logo"><span>logo</span></div>
+            <div class="signModal__header__close"><span>close</span></div>
+          </div>
+        </div>
       </template>
       <template #content> <SignInContainer></SignInContainer></template
     ></SignLayout>
@@ -23,5 +28,39 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-// @import "@/assets/styles/common.scss";
+#signModal__header__wrap {
+  width: 100%;
+  position: fixed;
+  top: 17px;
+  .signModal__header__container {
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    .signModal__header__logo {
+      @extend .backgroundImage--center;
+      width: 58.1px;
+      height: 20.1px;
+      background-image: url("../../assets/images/406.svg");
+      span {
+        @extend .txt-hidden;
+      }
+    }
+    .signModal__header__close {
+      @extend .backgroundImage--center;
+      width: 20px;
+      height: 20px;
+      position: absolute;
+      background-image: url("../../assets/images/372.svg");
+      right: 16px;
+      top: 50%;
+      transform: translate(0, -50%);
+
+      span {
+        @extend .txt-hidden;
+      }
+    }
+  }
+}
 </style>
