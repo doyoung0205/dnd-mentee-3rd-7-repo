afc9b7805fb0b33fb8bf859e6d6b2587df161c41
KDY <doyoung0205@naver.com>
2020-08-06 22:12:14 +0900

    FEAT add signIn 골격

---
+++ frontend/src/views/SignInPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="SignModal">
+    asdfsd
+    <SignInContainer></SignInContainer>
+  </div>
+</template>
+
+<script lang="ts">
+import SignInContainer from "@/components/sign/SignInContainer.vue";
+import Vue from "vue";
+export default Vue.extend({
+  name: "asda",
+  components: {
+    SignInContainer
+  }
+});
+</script>
+
+<style></style>
