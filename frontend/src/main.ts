bbbd48bc356c8eac6564c2e97e1a911729d4ceaf
KDY <doyoung0205@naver.com>
2020-08-27 17:56:17 +0900

    [ENH] ADD 웹 버전 메인 화면 마크업

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,20 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import {
+  Swiper as SwiperClass,
+  Pagination,
+  Mousewheel,
+  Autoplay,
+  Navigation
+} from "swiper/core";
+import getAwesomeSwiper from "vue-awesome-swiper/dist/exporter";
+
+// Swiper modules
+SwiperClass.use([Pagination, Mousewheel, Autoplay, Navigation]);
+
+// Global use
+Vue.use(getAwesomeSwiper(SwiperClass));
 
 Vue.config.productionTip = false;
 
