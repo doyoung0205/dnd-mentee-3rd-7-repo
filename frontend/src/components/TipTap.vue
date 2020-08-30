8109a1870f469dd6673cbdf82a0965a9c4cd7cf5
limaries30 <limaries30@gmail.com>
2020-08-30 20:22:46 +0900

    FEAT tiptap

---
+++ frontend/src/components/TipTap.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="editor">
+    <editor-menu-bar v-slot="{ commands, isActive }" :editor="editor">
+      <div class="menubar">
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bold() }"
+          @click="commands.bold"
+        >
+          <font-awesome-icon icon="bold" />
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.italic() }"
+          @click="commands.italic"
+        >
+          <font-awesome-icon icon="italic" />
+        </button>
+
+        <!-- 
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.strike() }"
+          @click="commands.strike"
+        >
+          <icon name="strike" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.underline() }"
+          @click="commands.underline"
+        >
+          <icon name="underline" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code() }"
+          @click="commands.code"
+        >
+          <icon name="code" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.paragraph() }"
+          @click="commands.paragraph"
+        >
+          <icon name="paragraph" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+          @click="commands.heading({ level: 1 })"
+        >
+          H1
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+          @click="commands.heading({ level: 2 })"
+        >
+          H2
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+          @click="commands.heading({ level: 3 })"
+        >
+          H3
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bullet_list() }"
+          @click="commands.bullet_list"
+        >
+          <icon name="ul" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.ordered_list() }"
+          @click="commands.ordered_list"
+        >
+          <icon name="ol" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.blockquote() }"
+          @click="commands.blockquote"
+        >
+          <icon name="quote" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code_block() }"
+          @click="commands.code_block"
+        >
+          <icon name="code" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.horizontal_rule"
+        >
+          <icon name="hr" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.undo"
+        >
+          <icon name="undo" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.redo"
+        >
+          <icon name="redo" />
+        </button> -->
+      </div>
+    </editor-menu-bar>
+
+    <editor-content class="editor__content" :editor="editor" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Editor, EditorContent, EditorMenuBar } from "tiptap";
+import { Bold, Italic } from "tiptap-extensions";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBold, faItalic } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faBold, faItalic);
+
+export default Vue.extend({
+  // mounted() {
+  //   this.editor = new Editor({
+  //     content: '<p>This is just a boring paragraph</p>'
+  //   })
+  // },
+  components: {
+    EditorContent,
+    EditorMenuBar,
+    FontAwesomeIcon
+  },
+  data() {
+    return {
+      editor: new Editor({
+        content: "<p>This is just a boring paragraph</p>",
+        extensions: [new Bold(), new Italic()]
+      })
+    };
+  },
+  beforeDestroy() {
+    // Always destroy your editor instance when it's no longer needed
+    this.editor.destroy();
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/tiptap/sass/main.scss";
+</style>
