7a45c94fbdeb41cac7aabbe2223106da8bba630f
KDY <doyoung0205@naver.com>
2020-09-01 15:00:25 +0900

    [FEAT]  vue meta 플러그인 연동

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,12 +11,20 @@ import {
   Navigation
 } from "swiper/core";
 import getAwesomeSwiper from "vue-awesome-swiper/dist/exporter";
+import VueMeta from "vue-meta";
 
 // Swiper modules
 SwiperClass.use([Pagination, Mousewheel, Autoplay, Navigation]);
 
 // Global use
 Vue.use(getAwesomeSwiper(SwiperClass));
+
+Vue.use(VueMeta, {
+  // 컴포넌트 옵션 이름 변경 (예: 'head')asd
+  keyName: "metaInfo",
+  // 태그를 관찰하기 위해 추가하는 속성 이름 vue-meta
+  attribute: "data-vue-meta"
+});
 
 Vue.config.productionTip = false;
 
