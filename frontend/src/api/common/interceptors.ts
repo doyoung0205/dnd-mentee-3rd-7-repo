3265254ba67e435e36b4887807697fcd8a88b9f2
limaries30 <limaries30@gmail.com>
2020-09-07 17:02:28 +0900

    ADD Refresh Token 적용 1. intercepter 에 access token 만료되면 refresh token 요청

--- frontend/src/api/common/interceptors.ts
+++ frontend/src/api/common/interceptors.ts
@@ -1,17 +1,39 @@
 import { AxiosInstance, AxiosRequestConfig } from "axios";
 import { SignState } from "@/store/sign/types";
 import { sign } from "@/store/sign/index";
+import { getRefToken } from "@/api/token";
+import { TokenRef, DecodedToken } from "@/api/token/type";
+import JwtDecode from "jwt-decode";
+import {
+  getAuthFromCookie,
+  saveAuthToCookie,
+  saveRefreshToLocal
+} from "@/utils/cookies";
 
 export function setInterceptors(instance: AxiosInstance) {
   // Add a request interceptor
   instance.interceptors.request.use(
-    function(config: AxiosRequestConfig) {
+    async function(config: AxiosRequestConfig) {
       // Do something before request is sent
       // console.log(config);
       console.log(
         "(sign.state as SignState).token;",
         (sign.state as SignState).token
       );
+
+      const token = (sign.state as SignState).token;
+      const decodedToken = JwtDecode(token) as DecodedToken;
+      const now = new Date().toString();
+      const expDate = new Date(decodedToken["exp"] * 1000).toString();
+
+      if (expDate < now) {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const data = { refresh: refreshToken } as TokenRef;
+        const newToken = await getRefToken(data);
+
+        saveAuthToCookie(newToken.data.access);
+        saveRefreshToLocal(newToken.data.refresh);
+      }
 
       config.headers.Authorization =
         "Bearer " + (sign.state as SignState).token;
@@ -36,5 +58,25 @@ export function setInterceptors(instance: AxiosInstance) {
       return Promise.reject(error);
     }
   );
+
+  // instance.interceptors.response.use(
+  //   function(response) {
+  //     // Any status code that lie within the range of 2xx cause this function to trigger
+  //     // Do something with response data
+  //     return response;
+  //   },
+  //   async function(error) {
+  //     const originalRequest = error.config;
+  //     if (error.response.status === 403 && !originalRequest._retry) {
+  //       originalRequest._retry = true;
+  //       const access_token = await refreshAccessToken();
+  //       axios.defaults.headers.common["Authorization"] =
+  //         "Bearer " + access_token;
+  //       return axiosApiInstance(originalRequest);
+  //     }
+  //     return Promise.reject(error);
+  //   }
+  // );
+
   return instance;
 }
